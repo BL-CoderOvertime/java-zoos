9a013917f71d5d7de138eb35a4de9614677a18e4
BL-CoderOvertime <brandonlively@live.com>
2019-06-12 13:44:58 -0400

    Setup ZooServices, ZooServersImpl

--- src/main/kotlin/com/wkdrabbit/zoo/repos/ZooRepository.kt
+++ src/main/kotlin/com/wkdrabbit/zoo/repos/ZooRepository.kt
@@ -1,7 +1,22 @@
 package com.wkdrabbit.zoo.repos
 
 import com.wkdrabbit.zoo.model.Zoo
+import com.wkdrabbit.zoo.views.CountAnimalsInZoo
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import java.util.ArrayList
+
+
 
 interface ZooRepository : CrudRepository<Zoo, Long>{
+
+
+    @get:Query(value = "SELECT s.zooId, zooName, count(s.animalId) as countanimals FROM zooanimals s INNER JOIN zoo c on s.zooid = c.zooid GROUP BY s.zooid, c.zooname", nativeQuery = true)
+    val getCountAnimalsInZoo: ArrayList<CountAnimalsInZoo>
+
+
+    @Modifying
+    @Query(value = "DELETE FROM zooanimals WHERE zooid = :zooid" , nativeQuery = true)
+    fun deleteZooFromZooanimals(zooid : Long)
 }
---
+++ src/main/kotlin/com/wkdrabbit/zoo/services/ZooServiceImpl.kt
@@ -0,0 +1,39 @@
+package com.wkdrabbit.zoo.services
+
+import com.wkdrabbit.zoo.model.Zoo
+import com.wkdrabbit.zoo.repos.ZooRepository
+import com.wkdrabbit.zoo.views.CountAnimalsInZoo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.util.ArrayList
+import javax.persistence.EntityNotFoundException
+import javax.transaction.Transactional
+
+@Service(value = "zooService")
+class ZooServiceImpl : ZooService {
+
+    @Autowired
+    val zooRepos : ZooRepository? = null
+
+
+    override fun findAll(): ArrayList<Zoo> {
+        var list = ArrayList<Zoo>()
+        //TODO: Fix this
+        zooRepos!!.findAll().iterator().forEachRemaining(list::add)
+    }
+
+
+    @Transactional
+    override fun delete(id: Long) {
+        if(zooRepos!!.findById(id).isPresent){
+            zooRepos!!.deleteZooFromZooanimals(id)
+            zooRepos!!.deleteById(id)
+        } else{
+            throw EntityNotFoundException(id.toString())
+        }
+    }
+
+    override fun getCountAnimalsInZoo(): ArrayList<CountAnimalsInZoo> {
+        return zooRepos!!.getCountAnimalsInZoo
+    }
+}
